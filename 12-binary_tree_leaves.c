10903868c60b63c8f08a26c2b2cb80031bb333bc
AmineCoder30 <99543252+AmineCoder30@users.noreply.github.com>
2023-08-30 13:05:23 +0100

    Add files via upload

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: our tree
+ * Return: leaves's number
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_is_leaf(tree))
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
